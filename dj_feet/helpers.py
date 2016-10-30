2368061647e35f75cb9a503aec33c32b1ae47a1c
Thomas Schaper <thomas+uva@libremail.nl>
2016-10-31 00:59:09 +0100

    Added the first helper functions

---
+++ dj_feet/helpers.py
@@ -0,0 +1,12 @@
+from collections import namedtuple
+
+SongStruct = namedtuple("SongStruct", "file_location start_pos end_pos")
+
+def get_all_subclasses(baseclass):
+    todo = [baseclass]
+    res = set()
+    while todo:
+        for cls in todo.pop().__subclasses__():
+            res.add(cls)
+            todo.append(cls)
+    return res
