6f3f63d834bc50a0342d183c575ed61b73361baf
Thomas Schaper <thomas+uva@libremail.nl>
2016-11-07 00:00:59 +0100

    Added a SimplePicker class.
    This class simple choices a random song and removes it from the list to choose
    from.

--- dj_feet/pickers.py
+++ dj_feet/pickers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from .helpers import SongStruct
+import os
+import random
 
 
 class Picker:
@@ -10,3 +12,18 @@ class Picker:
     def get_next_song(self, user_feedback):
         """Return a SongStruct for the next song that should be used"""
         raise NotImplementedError("This should be overridden")
+
+
+class SimplePicker(Picker):
+    def __init__(self, song_folder):
+        self.song_folder = song_folder
+        self.song_files = [f for _, __, f in os.walk(song_folder)][0]
+
+    def get_next_song(self, user_feedback):
+        next_song = ""
+        while not os.path.isfile(os.path.join(self.song_folder, next_song)):
+            if not self.song_files:
+                raise ValueError("There are no songs left")
+            next_song = random.choice(self.song_files)
+            self.song_files.remove(next_song)
+        return next_song
