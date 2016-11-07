3f03371bdd23e40a2244e9805313a8fd1fc1bc41
Thomas Schaper <thomas+uva@libremail.nl>
2016-11-07 02:01:36 +0100

    Done with simple non beatdetecting merger, test are a todo

--- dj_feet/transitioners.py
+++ dj_feet/transitioners.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from pydub import AudioSegment
 from .helpers import SongStruct
+from tempfile import NamedTemporaryFile
 
 
 class Transitioner:
@@ -17,3 +19,32 @@ class Transitioner:
         """
         raise NotImplementedError(
             "This function should be overridden by the subclass")
+
+
+class SimpleTransitioner(Transitioner):
+    def __init__(self, output, merge_time=100):
+        self.output = output
+        self.merge_time = merge_time
+
+    def merge(self, prev_song, next_song):
+        next_segment = AudioSegment.from_file(next_song.file_location)[
+            next_song.start_pos:next_song.end_post]
+        if prev_song is None:
+            return next_segment[:-self.merge_time]
+        prev_segment = AudioSegment.from_file(prev_song.file_location)[
+            prev_song.start_pos:prev_song.end_post]
+        return prev_segment[-self.merge_time:].append(
+            next_segment[:-self.merge_time],
+            crossfade=self.merge_time)
+
+    def write_sample(self, sample):
+        f_name = None
+        with NamedTemporaryFile("w+b", suffix=".mp3", delete=False) as f:
+            sample.export(f.name, format="mp3")
+            f_name = f.name
+        with open(f_name, 'rb') as input_f, open(self.output, 'wb') as output:
+            buf = True
+            while buf:
+                buf = input_f.read(4096)
+                if buf:
+                    output.write(buf)
--- tests/test_transitioners.py
+++ tests/test_transitioners.py
@@ -15,9 +15,26 @@ def transitioner_base():
     yield transitioners.Transitioner()
 
 
+@pytest.fixture(params=[transitioners.SimpleTransitioner])
+def all_transitioners(request):
+    yield request.param
+
+
 def test_base_transitioner(transitioner_base):
     assert isinstance(transitioner_base, transitioners.Transitioner)
     with pytest.raises(NotImplementedError):
         transitioner_base.merge(None, None)
     with pytest.raises(NotImplementedError):
         transitioner_base.write_sample(None)
+
+
+def test_all_transitioners(all_transitioners):
+    assert issubclass(all_transitioners, transitioners.Transitioner)
+
+    merge = all_transitioners.merge
+    assert callable(merge)
+    assert merge.__code__.co_argcount == 3
+
+    write_sample = all_transitioners.write_sample
+    assert callable(write_sample)
+    assert write_sample.__code__.co_argcount == 2
