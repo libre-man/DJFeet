6b3d307fc3bfb7ef72007df3576f519ac50f56a8
Devin Hillenius <devinhillenius@quicknet.nl>
2016-11-21 23:30:36 +0100

    Some minor changes

--- dj_feet/song.py
+++ dj_feet/song.py
@@ -3,20 +3,22 @@ from pydub import AudioSegment
 # Step size of streaming in miliseconds
 STEP_SIZE = 30000
 
+
 class Song:
     def __init__(self, file_location, start_pos, progress=0):
-        self.file_location = file_location
         self.start_pos = start_pos
         self.progress = progress
+        self.audio = AudioSegment.from_file(next_song.file_location)
 
-    def add_progress(addition=1):
+
+    def add_progress(self, addition=1):
         self.progress += addition
 
-    def get_next_segment():
+
+    def get_next_segment(self):
         """
         Return the next segment of the sample. The length of this segment
         is defined in the STEP_SIZE constant.
         """
-        return AudioSegment.from_file(next_song.file_location)
-               [self.start_pos + STEP_SIZE * self.progress:
-                self.start_pos + STEP_SIZE * (self.progress + 1)]
+        return self.audio[self.start_pos + STEP_SIZE * self.progress:
+            self.start_pos + STEP_SIZE * (self.progress + 1)]
