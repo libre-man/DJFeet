944aef880ef0d7ba02b71c2f6430ae52f6ebb7c7
Devin Hillenius <devinhillenius@quicknet.nl>
2016-11-22 19:11:23 +0100

    Fucking pep8 fixed

--- dj_feet/song.py
+++ dj_feet/song.py
@@ -1,6 +1,6 @@
 import librosa
 import numpy as np
-# Step size of streaming in seconds
+
 
 class Song:
     def __init__(self, file_location, curr_frame):
@@ -9,9 +9,8 @@ class Song:
         # Load the sample from the given location
         self.time_series, self.sampling_rate = librosa.load(file_location)
         # Get the beat track and BPM
-        self.tempo, self.beat_track = librosa.beat.beat_track(self.time_series,
-                                                            self.sampling_rate)
-
+        self.tempo, self.beat_track =
+        librosa.beat.beat_track(self.time_series, self.sampling_rate)
 
     def next_segment(self, segment_size, begin=False):
         if begin:
@@ -22,14 +21,12 @@ class Song:
             self.curr_frame += segment_size
 
         start, end = librosa.core.time_to_frames(time_vector,
-                                                  self.sampling_rate)
+                                                 self.sampling_rate)
         return (start, end)
-
 
     def time_delta(self, start_frame, end_frame):
         return librosa.frames_to_time(np.array(start_frame, end_frame),
                                       self.sampling_rate)
-
 
     def frame_to_segment_time(self, segment_size, prev_time, start_frame):
         time_diff = segment_size - prev_time
