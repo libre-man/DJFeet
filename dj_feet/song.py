13ea80db458f6734e73f7a2ad66db5dbb133b12a
Thomas Schaper <thomas+uva@libremail.nl>
2016-11-23 12:09:13 +0100

    Fixed style for song.py

--- dj_feet/song.py
+++ dj_feet/song.py
@@ -9,8 +9,8 @@ class Song:
         # Load the sample from the given location
         self.time_series, self.sampling_rate = librosa.load(file_location)
         # Get the beat track and BPM
-        self.tempo, self.beat_track =
-        librosa.beat.beat_track(self.time_series, self.sampling_rate)
+        self.tempo, self.beat_track = librosa.beat.beat_track(
+            self.time_series, self.sampling_rate)
 
     def next_segment(self, segment_size, begin=False):
         if begin:
@@ -25,11 +25,11 @@ class Song:
         return (start, end)
 
     def time_delta(self, start_frame, end_frame):
-        return librosa.frames_to_time(np.array(start_frame, end_frame),
-                                      self.sampling_rate)
+        return librosa.frames_to_time(
+            np.array(start_frame, end_frame), self.sampling_rate)
 
     def frame_to_segment_time(self, segment_size, prev_time, start_frame):
         time_diff = segment_size - prev_time
-        frame_delta = librosa.time_to_frames(np.array(0, time_diff),
-                                             self.sampling_rate)
+        frame_delta = librosa.time_to_frames(
+            np.array(0, time_diff), self.sampling_rate)
         return start_frame + frame_delta
