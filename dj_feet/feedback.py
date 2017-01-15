bb86dd1b54a5dfbe56dd9804c27c237df98d5f04
Thomas Schaper <thomas@libremail.nl>
2017-01-15 21:41:27 +0100

    Fix bug in the `percentage_liked' feedback function.
    There were bugs in this method and it was non conforming to the API and
    naming scheme. As such it did not work as intended.

--- dj_feet/feedback.py
+++ dj_feet/feedback.py
@@ -1,20 +1,21 @@
-def percentage_liked(controller_dict, segment_size):
+def feedback_percentage_liked(controller_dict):
     """
     Controller dictionary should be in the following format:
-        {client_id : time / None}
+        {feedback: {client_id : time / None}}
             in which time shows how much time (in seconds) after the latest
             transition the client left the channel. If the client_id is still
             on the channel, time = None.
     """
     total = 0
-    on_channel = 0
     channel_switches = 0
-    for _, time in controller_dict.items():
-        total += 1
 
+    for _, time in controller_dict['feedback'].items():
+        # We ignore all people that left the channel within 1 second of our
+        # merge as they did not react to this merge in our opinion.
         if time is None:
-            on_channel += 1
-        else if time > 1:
-            channel_switches += 1 / (time + 1/9) + 0.1
+            total += 1
+        if time is not None and time > 1:
+            channel_switches += 1 / (time + (1 / 9)) + 0.1
+            total += 1
 
     return (total - channel_switches) / total
