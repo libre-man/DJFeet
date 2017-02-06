80cbd9a1829fcac1ee314dc7f1a61d582853f210
Thomas Schaper <thomas@libremail.nl>
2017-02-06 23:25:06 +0100

    Fix song docs

--- dj_feet/song.py
+++ dj_feet/song.py
@@ -12,6 +12,13 @@ class Song:
     multiple functions to convert different time units.
     """
     def __init__(self, file_location, process=True):
+        """
+        :param string file_location: The path to the wav file to use as base
+                                     for this song.
+        :param bool process: Indicate if we should process the song in the
+                             ``__init__`` method. This may be really slow, but
+                             methods may not work without doing this.
+        """
         self.file_location = file_location
         self.curr_time = 0
         self.time_series = self.sampling_rate = None
@@ -24,6 +31,9 @@ class Song:
 
         Load the song using librosa and find the beat track to set class fields
         for future use. Please note that this operation may take a long time!
+
+        :returns: Always returns None
+        :rtype: None
         """
         # Load the sample from the given location
         self.time_series, self.sampling_rate = librosa.load(self.file_location)
@@ -33,22 +43,20 @@ class Song:
         self.beat_track = librosa.core.frames_to_samples(beat_track_frames)
 
     def next_segment(self, segment_size, begin=False):
-        """Get the next sector starting from the `begin` or `curr_time` of
-        `segment_size` long.
+        """Get the next sector starting from the ``begin`` or ``curr_time`` of
+        ``segment_size`` long.
 
-        Get the next sector (a part of the song of given `segment_size` long)
-        of the song if `begin` is False. Get the first `segment_size` of the
-        song otherwise.
+        Get the next sector (a part of the song of given ``segment_size`` long)
+        of the song if ``begin`` is False. Get the first ``segment_size`` of
+        the song otherwise.
 
-        :param segment_size: The length of the next sector to get in seconds.
-        :type segment_size: int
-        :param begin: Whether the sector should be gotten from the song's
-                      current time or from the beginning of the song.
-                      (default=False)
-        :type begin: bool
+        :param int segment_size: The length of the next sector to get in
+                                 seconds.
+        :param bool begin: Whether the sector should be gotten from the song's
+                           current time or from the beginning of the song.
         :returns: A tuple with the sample index of respectively the first and
                   last sample of the next sector.
-        :rtype: (int, int)
+        :rtype: tuple(int, int)
         """
         if begin:
             time_vector = np.array([0, segment_size])
@@ -66,10 +74,8 @@ class Song:
         Find the time delta (in seconds) between two given sample indices. That
         is the difference in time (seconds) between the given sample indices.
 
-        :param start_frame: The index of the first sample.
-        :type start_frame: int
-        :param end_frame: The index of the last sample.
-        :type end_frame: int
+        :param int start_frame: The index of the first sample.
+        :param int end_frame: The index of the last sample.
         :returns: The difference in seconds between both given samples.
         :rtype: int
         """
@@ -78,22 +84,20 @@ class Song:
         return d2 - d1
 
     def frame_to_segment_time(self, segment_size, start_frame):
-        """Give a sample `segement_size` amount of seconds away from the given
-        `start_frame`.
+        """Give a sample ``segement_size`` amount of seconds away from the given
+        ``start_frame``.
 
-        Find the index of a sample that is exactly `segment_size` seconds after
-        the given `start_frame`. No guarantees are made that this given sample
-        index is a valid index for the given song, this should be checked
-        manually.
+        Find the index of a sample that is exactly ``segment_size`` seconds
+        after the given ``start_frame``. No guarantees are made that this given
+        sample index is a valid index for the given song, this should be
+        checked manually.
 
-        :param segment_size: The time in seconds between the given
-                             `start_frame` and the returned sample index.
-        :type segment_size: int
-        :param start_frame: The index of the sample to find the sample
-                            `segment_size` seconds away from.
-        :type start_frame: int
-        :returns: A sample index that is `segment_size` seconds after the given
-                  `start_frame`.
+        :param int segment_size: The time in seconds between the given
+                             ``start_frame`` and the returned sample index.
+        :param int start_frame: The index of the sample to find the sample
+                            ``segment_size`` seconds away from.
+        :returns: A sample index that is ``segment_size`` seconds after the
+                  given ``start_frame``.
         :rtype: int
         """
         time_diff = segment_size
@@ -102,19 +106,17 @@ class Song:
         return start_frame + (d2 - d1)
 
     def beat_tracks_in_segment(self, seg_start, seg_end):
-        """Get all beats between the `seg_start` and the `seg_end` frames.
+        """Get all beats between the ``seg_start`` and the ``seg_end`` frames.
 
-        Given two sample indices `seg_start` and `seg_end` find all current
+        Given two sample indices ``seg_start`` and ``seg_end`` find all current
         song's beats that are in between this range (inclusive).
 
-        :param seg_start: The sample index indicating the beginning of the
-                          range to find beats on.
-        :type seg_start: int
-        :param seg_end: The sample index indicating the end of the range to
-                        find beats on.
-        :type seg_end: int
+        :param int seg_start: The sample index indicating the beginning of the
+                              range to find beats on.
+        :param int seg_end: The sample index indicating the end of the range to
+                            find beats on.
         :returns: A list containing all indices of beats in the given range.
-        :rtype: [int]
+        :rtype: numpy.array(int)
         """
         beat_tracks = []
         for bt in self.beat_track:
@@ -125,18 +127,17 @@ class Song:
 
     def segment_size_left(self, segment_size):
         """
-        Returns true if there's more or equal to `segment_size` time left in
-        the song starting from `curr_time`, false if not.
+        Returns true if there's more or equal to ``segment_size`` time left in
+        the song starting from ``curr_time``, false if not.
 
-        Finds whether another segment of `segment_size` seconds long is
+        Finds whether another segment of ``segment_size`` seconds long is
         available in the song or that the song is too close to it's end. It is
         assumed that the song's current position is correctly stored in
-        the `cur_time` variable (which it should).
+        the ``cur_time`` variable (which it should).
 
-        :param segment_size: The length in seconds of a segment.
-        :type segment_size: int
+        :param int segment_size: The length in seconds of a segment.
         :returns: True if the song's current position is more than
-                  `segment_size` seconds from the end. False otherwise.
+                  ``segment_size`` seconds from the end. False otherwise.
         :rtype: boolean
         """
         curr_sample = librosa.core.time_to_samples(np.array(self.curr_time),
